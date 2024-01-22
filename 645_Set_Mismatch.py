38eb71bfdd778fe097bcf647f033df8d5b5fc0fc
AlienOwl <25430207+eslamNova@users.noreply.github.com>
2024-01-22 05:22:32 +0200

    Create 645_Set_Mismatch.py
    Easy Problem

---
+++ 645_Set_Mismatch.py
@@ -0,0 +1,17 @@
+#Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        d = Counter(nums)
+        m = 0
+        r = 0
+        for i in range(1, len(nums)+1):
+            if i not in nums:
+                m = i
+            if d[i] == 2:
+                r = i
+            if m != 0 and r != 0:
+                break
+        return [r,m]
