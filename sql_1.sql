7a54e328ff1452af56d83d0635ce6c4290ba4207
Islam Ashraf <islam.ashraf@ivsystems-eg.com>
2024-07-08 09:59:40 +0300

    is this working?

--- sql_1.sql
+++ sql_1.sql
@@ -1,3 +1,7 @@
 Select Product_id from products Where low_fats = 'Y' and recyclable = 'Y'
 
 Select name from Customer where referee_id != 2 or referee_id is null
+
+
+Select name, area, population from World WHERE area >= 3000000 or population >= 25000000
+
