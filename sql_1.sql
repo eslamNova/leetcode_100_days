6b7711ad451c01dafdb870f18021d09493376e13
eslamNova <islam.a.i@outlook.com>
2024-07-12 23:50:31 +0300

    last min

--- sql_1.sql
+++ sql_1.sql
@@ -5,3 +5,6 @@ Select name from Customer where referee_id != 2 or referee_id is null
 
 Select name, area, population from World WHERE area >= 3000000 or population >= 25000000
 
+Select DISTINCT author_id as id from Views WHERE author_id = viewer_id ORDER BY id
+
+
