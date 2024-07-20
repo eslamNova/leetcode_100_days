92841f3e536e68ff7e88d12e4de23117b173fd44
eslamNova <islam.a.i@outlook.com>
2024-07-20 16:12:34 +0300

    lazy

--- sql_1.sql
+++ sql_1.sql
@@ -32,3 +32,6 @@ CROSS JOIN Subjects sub
 LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
 GROUP BY s.student_id, s.student_name, sub.subject_name
 ORDER BY s.student_id, sub.subject_name;
+
+
+SELECT teacher_id, count(distinct subject_id) as cnt from Teacher group by teacher_id
