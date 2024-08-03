54d524694a9829c2fd001192648ac17702f04401
eslamNova <islam.a.i@outlook.com>
2024-08-03 12:33:46 +0300

    neetcode0?

---
+++ end_dec.py
@@ -0,0 +1,14 @@
+# O(N)
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "empty"
+        return('-'.join(strs))
+
+    def decode(self, s: str) -> List[str]:
+        if s == "empty":
+            return []
+
+        return s.split('-')
