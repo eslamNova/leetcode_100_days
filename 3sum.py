c66cef22b66691b5e7cf864878838a66d7e126c7
eslamNova <islam.a.i@outlook.com>
2024-08-18 17:48:10 +0300

    needcode012

---
+++ 3sum.py
@@ -0,0 +1,29 @@
+# O(N^2)
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = [] 
+        nums.sort()
+
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i-1]:
+                continue
+            
+            l, r = i+1, len(nums)-1
+
+            while l < r:
+                s3 = n + nums[l] + nums[r]
+
+                if s3 > 0:
+                    r -= 1
+                elif s3 < 0:
+                    l += 1
+                else:
+                    ans.append([n, nums[l], nums[r]])
+                    l += 1
+
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return ans
+                    
+        
