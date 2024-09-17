37134b0af0aa3783b190b2079fe9acdf333174b9
eslamnova <islam.a.i@outlook.com>
2024-09-17 15:34:49 +0300

    day2

---
+++ uncomn.py
@@ -0,0 +1,8 @@
+# O(N)
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+
+        st = s1 + " " + s2
+        c = Counter(st.split(" "))
+        return [t for t in c if c[t] == 1]
+        
