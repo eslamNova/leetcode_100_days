d34e222c566dd7ab37ee76f92c2a41e4cd7d0bf8
eslamnova <islam.a.i@outlook.com>
2024-11-07 14:25:05 +0300

    easy

---
+++ xmat.py
@@ -0,0 +1,18 @@
+# O(N2)
+class Solution:
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        n = len(grid)
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    if grid[i][j] == 0:
+                        return False
+                elif i+j == n-1:
+                    if grid[i][j] == 0:
+                        return False
+                else:
+                    if grid[i][j] !=0 :
+                        return False
+        return True
+                    
+        
