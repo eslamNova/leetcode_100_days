f35c767c50042b29d9b15f9a6ff970a8d1872635
eslamnova <islam.a.i@outlook.com>
2025-01-26 16:31:56 +0200

    aa

---
+++ arra_del.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for n in nums:
+            if n != val:
+                nums[j] = n
+                j += 1
+        return j
