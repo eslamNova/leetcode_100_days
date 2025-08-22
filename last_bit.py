260c635db8f4cda647f4477963e38dd757130837
eslamNova <islam.a.i@outlook.com>
2025-08-22 16:50:49 +0300

    QT

---
+++ last_bit.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        while i < len(bits) - 1:  # stop before last index
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+        return i == len(bits) - 1
+
+        
