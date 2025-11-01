303cc5b126e104f8ba1f2c3e39b5041b2a776ea9
eslamNova <islam.a.i@outlook.com>
2025-11-01 17:03:35 +0300

    AF

---
+++ del_from_linked_list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        d = ListNode(0)
+        d.next = head
+        cur  = d
+        f = set(nums)
+        while cur.next:
+
+            if cur.next.val in f:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+
+        return d.next
+
+        
