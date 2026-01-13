23f8cc9c320feac52b98eec94f7091dbe474ed51
eslamnova <islam.a.i@outlook.com>
2026-01-13 18:07:18 +0300

    ASDA

---
+++ panda_sort.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    df = cinema[(cinema["description"] != 'boring') & (cinema['id'] % 2 != 0)]
+    df = df.sort_values(by='rating', ascending=False)
+    return df
+    
