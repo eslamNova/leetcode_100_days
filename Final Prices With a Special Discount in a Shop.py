73e325b55856b9b47a6dcb81701f2cc0e16c9cdb
Islam Ashraf <25430207+eslamNova@users.noreply.github.com>
2026-05-24 16:26:05 +0300

    Implement finalPrices method to calculate discounts

---
+++ Final Prices With a Special Discount in a Shop.py
@@ -0,0 +1,16 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+
+        ans = []
+        for i in range(len(prices)):
+            p = prices[i]
+            d = False
+            for j in range(i+1,len(prices)):
+                if prices[j] <= prices[i]:
+                    ans.append(prices[i] - prices[j])
+                    d= True
+                    break
+            if not d:
+                ans.append(prices[i])
+        return ans
+        
